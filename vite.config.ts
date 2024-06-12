5420bf02824f1c65a4aaf61287bef2ca4b90ab92
Bowen Li <superdev@outlookcom.au>
2024-06-12 23:20:15 +1000

    use hashrouter

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/audco_payment/',  // Replace with your repository name
+  // base: '/audco_payment/',  // Replace with your repository name
 
 })
